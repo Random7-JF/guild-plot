da291a0cfd97c0afb25cbea2f2130442a978d8c7
Jon Fingas <jon.fingas@gmail.com>
2025-11-12 21:23:21 -0600

    added api endpoint

--- app/handlers/plots.go
+++ app/handlers/plots.go
@@ -1,7 +1,20 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/Random7-JF/guild-plot/db"
+)
 
 func Plots(w http.ResponseWriter, r *http.Request) {
+	// Set the Content-Type header to application/json
+	w.Header().Set("Content-Type", "application/json")
 
+	// Encode the struct to JSON and write it to the response body
+	if err := json.NewEncoder(w).Encode(db.GetAllPlots()); err != nil {
+		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
+		log.Printf("Error encoding JSON: %v", err)
+	}
 }
